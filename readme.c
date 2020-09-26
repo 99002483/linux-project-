e5e2754e0e08d40f0e645f323244e295a160c614
99002483 <71481320+99002483@users.noreply.github.com>
2020-09-26 13:51:23 +0530

    Update readme.c

--- readme.c
+++ readme.c
@@ -41,10 +41,3 @@ Give command "make"
 
 
 
-BADGES 
-
-
-
-![CI](https://github.com/99002483/linux-project-/workflows/CI/badge.svg)
-
-![CI](https://github.com/99002483/linux-project-/workflows/CI/badge.svg)
